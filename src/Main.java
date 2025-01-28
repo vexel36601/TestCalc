9aa9c9fe6095658c51762762beb455a3234ababc
vexel36601 <vexel.xx@icloud.com>
2025-01-28 17:10:06 +0700

    1

---
+++ src/Main.java
@@ -0,0 +1,49 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+    Scanner sc = new Scanner(System.in);
+     System.out.println("Введи выражение(например: 2 + 2)");
+      String input = sc.nextLine();
+       String result = calc(input);
+        System.out.println("Результат: " + result);
+    }
+
+    static String calc (String input) throws Exception {
+        String[] parts = input.split(" ");
+        if (parts.length != 3) {
+            throw new Exception("Ошибка: Неверный формат ввода.");
+        }
+        int a = Integer.parseInt(parts[0]); // 1 число из String в int
+        String operator = parts[1]; // Оператор ("+", "-", "*", "/")
+        int b = Integer.parseInt(parts[2]); // 2 число из String в int
+
+        // Проверка, что оба числа от 0 до 10
+        if (a < 0 || a > 10 || b < 0 || b > 10) {
+            throw new Exception("Ошибка: Числа должны быть от 0 до 10.");
+        }
+        // Расчет выражения
+        int result ;
+        switch (operator){
+            case"+":
+                result = a + b;
+                  break;
+            case"-":
+                result = a - b;
+                  break;
+            case"*":
+                result = a * b;
+                  break;
+            case"/":
+                if(b ==0){
+            throw new Exception("Ошибка: деление на ноль.");
+        }
+                result = a / b;
+                  break;
+
+            default:
+            throw new Exception("Ошибка: Используйте +, -, *, /.");
+        }
+        return String.valueOf(result);
+    }
+}
